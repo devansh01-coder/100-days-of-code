e46f0cf60d9842c758f97bb673082f4e65f708ea
Devansh Aggarwal <devanshaggarwal3010@gmail.com>
2026-09-14 20:19:39 +0530

    Add files via upload

---
+++ 66.c
@@ -0,0 +1,37 @@
+//Insert an element in a sorted array at the appropriate position.
+#include <stdio.h>
+
+int main() {
+int n;
+printf("Enter number of elements: ");
+scanf("%d", &n);
+
+int arr[n + 1];
+
+printf("Enter %d elements in sorted order: ", n);
+for (int i = 0; i < n; i++) {
+scanf("%d", &arr[i]);
+}
+
+int key;
+printf("Enter the element to insert: ");
+scanf("%d", &key);
+
+int i = n - 1;
+
+while (i >= 0 && arr[i] > key) {
+arr[i + 1] = arr[i];
+i--;
+}
+
+arr[i + 1] = key;
+n++;
+
+printf("Array after insertion: ");
+for (int i = 0; i < n; i++) {
+printf("%d ", arr[i]);
+}
+printf("\n");
+
+return 0;
+}
